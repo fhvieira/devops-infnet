b2198a4c5a2896ed41d9ede2de60d0f389e10058
fhvieira <fhvieira@hotmail.com>
2023-04-10 21:31:25 -0300

    ultimos ajustes

---
+++ src/main/java/br/edu/infnet/cliente/config/LoggerConfig.java
@@ -0,0 +1,19 @@
+package br.edu.infnet.cliente.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration(proxyBeanMethods = false)
+public class LoggerConfig {
+
+    Logger log = LoggerFactory.getLogger(LoggerConfig.class);
+
+    @Bean
+    RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+}
